d9f4a96f3f783477cb3aab23cb70a2eccd73d02c
foussenitoure <tourefousseni1@gmail.com>
2020-10-11 01:41:23 +0000

    added GDAL on setting file

--- Home_KALALISO/settings.py
+++ Home_KALALISO/settings.py
@@ -129,3 +129,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+GDAL_LIBRARY_PATH = r'C:\OSGeo4W64\bin\gdal202'
