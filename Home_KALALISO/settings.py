5e092f9186e5f7716698b956441dc4cb0551ab6a
foussenitoure <tourefousseni1@gmail.com>
2020-10-11 02:04:29 +0000

    DeBug = false for setting file

--- Home_KALALISO/settings.py
+++ Home_KALALISO/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '1&-+9^&5htao6z_%+(-2%_1u#*st5ui%6&y&rm^(^qk#tq(r2t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['198.211.109.66']
 
