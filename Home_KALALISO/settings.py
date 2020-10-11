c171c26cef43d9794ed1c65872963d19f7d61943
foussenitoure <tourefousseni1@gmail.com>
2020-10-11 03:26:26 +0000

    Setting file for database config

--- Home_KALALISO/settings.py
+++ Home_KALALISO/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'eco',
-        'USER': 'postgres',
+        'USER': 'myprojectuser',
         'PASSWORD':'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
